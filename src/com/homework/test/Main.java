b5828444711dd599b077290d9f4d5bd3771bff17
Cartoshechka <skalatushkin@gmail.com>
2021-02-07 11:26:37 +0200

    updated Task 19

--- src/com/homework/test/Main.java
+++ src/com/homework/test/Main.java
@@ -1,9 +1,25 @@
 package com.homework.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println(String.format("Hello %s %s" ,  "world","!!!"));
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        int a = Integer.parseInt(reader.readLine()), b = Integer.parseInt(reader.readLine()), c = Integer.parseInt(reader.readLine());
+        String template;
+        if (a < 0 || b < 0 || c < 0)
+            template = "Warning! a = %d, b = %d, c = %d";
+        else
+            template = "a = %d, b = %d, c = %d";
+
+        System.out.println(String.format(template, a, b, c) );
 
     }
 }

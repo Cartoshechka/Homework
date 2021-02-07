751a6fa7a9d75e0c2f24fdd0121135a0ce8a8c0b
Cartoshechka <skalatushkin@gmail.com>
2021-02-07 11:23:08 +0200

    updated Task 19

---
+++ src/com/homework/test/Main.java
@@ -0,0 +1,9 @@
+package com.homework.test;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(String.format("Hello %s %s" ,  "world","!!!"));
+
+    }
+}

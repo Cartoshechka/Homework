b7ed251d1a4697bd1ca9a60a7df6d3e9641406fa
Cartoshechka <skalatushkin@gmail.com>
2021-02-17 19:37:21 +0200

    updated Task 23

--- src/com/homework/task23/Main.java
+++ src/com/homework/task23/Main.java
@@ -3,10 +3,11 @@ package com.homework.task23;
 import java.io.*;
 
 public class Main {
+    static User info;
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader switchReader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Choose the wariant of work (reader or writer)");
-        String input = reader.readLine();
+        String input = switchReader.readLine();
         while (!(input.isEmpty())) {
             ReaderNWriterEnum readerNWriter = ReaderNWriterEnum.valueOf(input.toUpperCase());
             switch (readerNWriter) {
@@ -22,46 +23,58 @@ public class Main {
         }
 
     }
-
     public static String reading() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String res = null;
         String sourceFileName;
-        while (true) try {
+        try {
             System.out.println("Type file name to get text from it");
             sourceFileName = reader.readLine();
             reader = new BufferedReader(new FileReader(sourceFileName));
-            res = reader.readLine();
-            break;
-
+            info = new User(reader.readLine());
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             System.out.println("Type file name one more time");
             sourceFileName = reader.readLine();
             reader = new BufferedReader(new FileReader(sourceFileName));
-            res = reader.readLine();
-            break;
+            info = new User(reader.readLine());
         }
-        return res;
+        return "Information in file: " + info.getInfo();
     }
 
     public static String writing() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (true) try {
+        BufferedReader writer = new BufferedReader(new InputStreamReader(System.in));
+        try {
             System.out.println("Type file name in which you want to write something");
-            OutputStream outputStream = new FileOutputStream(reader.readLine());
+            OutputStream outputStream = new FileOutputStream(writer.readLine());
             System.out.println("Type writing text");
-            String userText = reader.readLine();
+            info = new User(writer.readLine());
+            String userText = info.getInfo();
+            System.out.println("Are you sure about this text? " + userText);
+            System.out.println("Print Y or N");
+            String userAnswer = writer.readLine();
+            if (userAnswer.equals("N") || userAnswer.equals("n")) {
+                System.out.println("Print new text");
+                info = new User(writer.readLine());
+                userText = info.getInfo();
+            }
             outputStream.write(userText.getBytes());
-            break;
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
             System.out.println("Try to type file name again");
-            OutputStream outputStream = new FileOutputStream(reader.readLine());
+            OutputStream outputStream = new FileOutputStream(writer.readLine());
             System.out.println("Type writing text");
-            String userText = reader.readLine();
+            info = new User(writer.readLine());
+            String userText = info.getInfo();
+            System.out.println("Are you sure about this text? " + userText);
+            System.out.println("Print Y or N");
+            String userAnswer = writer.readLine();
+            if (userAnswer.equals("N") || userAnswer.equals("n")) {
+                System.out.println("Print new text");
+                info = new User(writer.readLine());
+                userText = info.getInfo();
+            }
             outputStream.write(userText.getBytes());
         }
-        return "Ready";
+        return "Written text: " + info.getInfo();
     }
 }
---
+++ src/com/homework/task23/User.java
@@ -0,0 +1,14 @@
+package com.homework.task23;
+
+public class User {
+    String info;
+
+    public User(String info) {
+        this.info = info;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+}

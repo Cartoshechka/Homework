8639c3fa45b530eba48abdaf70c7faeff2f24438
Cartoshechka <skalatushkin@gmail.com>
2021-02-18 18:06:33 +0200

    updated Task 11

--- src/com/homework/task11/Main.java
+++ src/com/homework/task11/Main.java
@@ -4,13 +4,13 @@ package com.homework.task11;
 public class Main {
 
     public static void main(String[] args) {
-        BaseProperty gilaya = new LivingArea(20.7, 100.25, 10);
-        BaseProperty torgovaya = new TradingArea(750.4, 5.80, 100);
-        BaseProperty proizvodstvo = new ProductionArea(4559.75, 54.84);
+        BaseProperty livingArea = new LivingArea(20.7, 100.25, 10);
+        BaseProperty tradingArea = new TradingArea(750.4, 5.80, 100);
+        BaseProperty productionArea = new ProductionArea(4559.75, 54.84);
 
-        System.out.println("Налог на жил площадь -> " + gilaya.taxCalculation());
-        System.out.println("Налог на тогр площадь -> " + torgovaya.taxCalculation());
-        System.out.println("Налог на производственную площадь -> " + proizvodstvo.taxCalculation());
+        System.out.println("Налог на жил площадь -> " + livingArea.taxCalculation());
+        System.out.println("Налог на тогр площадь -> " + tradingArea.taxCalculation());
+        System.out.println("Налог на производственную площадь -> " + productionArea.taxCalculation());
 
     }
 }

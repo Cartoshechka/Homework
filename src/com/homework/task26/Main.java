a424e45b27bdf279367f7052396059056911a60e
Cartoshechka <skalatushkin@gmail.com>
2021-02-22 15:22:35 +0200

    updated Task 26

--- src/com/homework/task26/Main.java
+++ src/com/homework/task26/Main.java
@@ -1,60 +1,44 @@
 package com.homework.task26;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Counter counter = new Counter();
+        Counter1 counter = new Counter1();
+        List<Thread> threads = new ArrayList<>();
+
         for (int i = 0; i < 10; i++) {
-            Thread thread = new Thread(counter);
-            System.out.println(thread.getName() + " starting");
-            thread.start();
-            System.out.println(thread.getName() + " joining");
-            thread.join();
+            Thread thread0 = new Thread(counter);
+            thread0.start();
+            System.out.println(thread0.getName() + " started");
+            threads.add(thread0);
         }
-        /*
-       for (int i = 0; i < 10; i++) {
-            Counter1 counter1 = new Counter1();
-            counter1.start();
-            System.out.println(counter1.getName() + " started");
-            counter1.join();
-            System.out.println(counter1.getName() + " joined");
-        }*/
+        for (Thread thr : threads) {
+            thr.join();
+            System.out.println(thr.getName() + " joined");
+        }
+        System.out.println("The result is " + Counter1.getCounter());
     }
 }
 
-class Counter implements Runnable {
-    private static int counter;
+class Counter1 implements Runnable {
+    private static Integer counter = 0;
 
     private static void nextCounter() {
         counter++;
     }
 
-    @Override
-    public void run() {
-        synchronized (this) {
-            while (counter != 1000) {
-                nextCounter();
-            }
-            System.out.println("The result is " + counter);
-        }
-    }
-}
-/*
-class Counter1 extends Thread {
-    private static int counter;
-
-    private static void nextCounter() {
-        counter++;
+    public static Integer getCounter() {
+        return counter;
     }
 
     @Override
     public void run() {
         synchronized (this) {
-            while (counter != 1000) {
+            for (int i = 0; i < 1000; i++) {
                 nextCounter();
             }
-            System.out.println("The result is "+ counter + " work done at: "  + getName());
         }
     }
 }
-*/
